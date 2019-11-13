b7364290aa9aa524bf52dedb335ffd019d695535
pifuchan <pifuchan@gmail.com>
2019-11-13 18:00:52 +0800

    修改读数据脚本

--- train.py
+++ train.py
@@ -13,15 +13,21 @@ from tokenizations.bpe_tokenizer import get_encoder
 
 
 def build_files(data_path, tokenized_data_path, num_pieces, full_tokenizer, min_length):
-    with open(data_path, 'r', encoding='utf8') as f:
+    # with open(data_path, 'r', encoding='utf8') as f:
+    #     print('reading lines')
+    #     lines = json.load(f)
+    #     lines = [line.replace('\n', ' [SEP] ') for line in lines]  # 用[SEP]表示换行, 段落之间使用SEP表示段落结束
+    lines = []
+    with open(data_path, 'r', encoding='utf8') as fr:
         print('reading lines')
-        lines = json.load(f)
-        lines = [line.replace('\n', ' [SEP] ') for line in lines]  # 用[SEP]表示换行, 段落之间使用SEP表示段落结束
+        for cur_line in fr:
+            lines.append(cur_line.strip().replace('\n', ' [SEP] '))
     all_len = len(lines)
+    print("训练语料的总量 = %d " % len(lines))
     if not os.path.exists(tokenized_data_path):
         os.mkdir(tokenized_data_path)
     for i in tqdm(range(num_pieces)):
-        sublines = lines[all_len // num_pieces * i: all_len // num_pieces * (i + 1)]
+        sublines = lines[all_len // num_pieces * i: all_len // num_pieces * (i + 1)]    # 取整除 - 返回商的整数部分（向下取整）
         if i == num_pieces - 1:
             sublines.extend(lines[all_len // num_pieces * (i + 1):])  # 把尾部例子添加到最后一个piece
         sublines = [full_tokenizer.tokenize(line) for line in sublines if
@@ -41,10 +47,10 @@ def build_files(data_path, tokenized_data_path, num_pieces, full_tokenizer, min_
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--device', default='0,1,2,3', type=str, required=False, help='设置使用哪些显卡')
-    parser.add_argument('--model_config', default='config/model_config_small.json', type=str, required=False,
+    parser.add_argument('--model_config', default='config/model_config.json', type=str, required=False,
                         help='选择模型参数')
-    parser.add_argument('--tokenizer_path', default='cache/vocab_small.txt', type=str, required=False, help='选择词库')
-    parser.add_argument('--raw_data_path', default='data/train.json', type=str, required=False, help='原始训练语料')
+    parser.add_argument('--tokenizer_path', default='cache/vocab.txt', type=str, required=False, help='选择词库')
+    parser.add_argument('--raw_data_path', default='data/train.txt', type=str, required=False, help='原始训练语料')
     parser.add_argument('--tokenized_data_path', default='data/tokenized/', type=str, required=False,
                         help='tokenized语料存放位置')
     parser.add_argument('--raw', action='store_true', help='是否先做tokenize')
